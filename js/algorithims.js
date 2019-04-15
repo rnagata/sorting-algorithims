c47264394a25b2a5fd7c51801cdb87e271105254
Ronald J Nagata Jr <junzo@Ronalds-MBP.lan1>
2019-04-15 00:46:58 -1000

    Merge Sort first pass

--- js/algorithims.js
+++ js/algorithims.js
@@ -1,6 +1,8 @@
 let unsortedArr = [10, 6, 2, 8, 3, 9, 1, 7, 4, 5, 0];
 let unsortedArr2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 let unsortedArr3 = [7, 9, 3, 4, 10, 8, 5, 6, 1, 2, 3];
+let unsortedArr4 = [10, 5, 3, 4, 100, 3333, 51];
+let unsortedArr5 = [34, 10, 57, 45, 9806, 298239824, 328723];
 
 // Quicksort, Bubblesort, Insertion Sort, Selection, Merge
 
@@ -30,11 +32,9 @@ function quickSort(numArr){
   return quickSort(lowValues).concat(pivot, quickSort(highValues));
 };
 
-console.log(`Quicksort Arr 1: ${unsortedArr} ----> ${quickSort(unsortedArr)}`);
-console.log(`Quicksort Arr 2: ${unsortedArr2} ----> ${quickSort(unsortedArr2)}`);
-console.log(`Quicksort Arr 3: ${unsortedArr3} ----> ${quickSort(unsortedArr3)}`);
+console.log(`Quicksort: ${unsortedArr} ----> ${quickSort(unsortedArr)}`);
 
-/*Bubble Sort:
+/* Bubble Sort:
 Steps through the unsorted array and compares adjacent values. The highest value
 moves right and upon reaching the final pair the process restarts, comparing one less
 element, which should hold the highest value from the preceeding pass. 
@@ -52,10 +52,78 @@ function bubbleSort(arr){
     }
     lastIndex--;
   }
-
+  
   return arr;
 }
 
-console.log(`Bubblesort Arr 1: ${unsortedArr} ----> ${bubbleSort(unsortedArr)}`);
-console.log(`Bubblesort Arr 2: ${unsortedArr2} ----> ${bubbleSort(unsortedArr2)}`);
-console.log(`Bubblesort Arr 3: ${unsortedArr3} ----> ${bubbleSort(unsortedArr3)}`);
+console.log(`Bubblesort: ${unsortedArr2} ----> ${quickSort(unsortedArr2)}`);
+
+/* Insertion Sort:
+Steps through the unsorted array, choosing one input element each repetition to place into the sorted
+section of the array. Shift other elements greater than the inserted value up.
+*/
+function insertionSort (items) {
+  for (var i = 0; i < items.length; i++) {
+    let valueToSort = items[i]
+    for (var compIndex = i - 1; compIndex > -1 && items[compIndex] > valueToSort; compIndex--) {
+      items[compIndex + 1] = items[compIndex];
+    }
+    items[compIndex + 1] = valueToSort;
+  }
+
+  return items;
+}
+
+console.log(`Insertion Sort: ${unsortedArr3} ----> ${quickSort(unsortedArr3)}`);
+
+/* Selection Sort:
+Iterates through the given array and finds the smallest value, then places that value to the beginning of 
+the array where it is considered part of a sorted sub-list. Process repeats until all unsorted values
+have moved to the sorted values, always picking the smallest value in the unsorted values each iteration.
+*/
+function selectionSort(arr, unsortedStart){
+  if (unsortedStart >= arr.length - 1){
+    return arr;
+  }
+  let unsortedItems = arr.slice(unsortedStart, arr.length);
+  let smallestValue = unsortedItems[0];
+  unsortedItems.forEach((element) => {
+    if (element < smallestValue){
+      smallestValue = element;
+    }
+  });
+  let prevSmallIndex = unsortedItems.indexOf(smallestValue);
+  let temp = unsortedItems[0];
+  unsortedItems[0] = unsortedItems[prevSmallIndex];
+  unsortedItems[prevSmallIndex] = temp;
+  arr.splice(unsortedStart, arr.length);
+  arr = arr.concat(unsortedItems);
+  return selectionSort(arr, ++unsortedStart);
+}
+
+console.log(`Selection Sort: ${unsortedArr4} ----> ${selectionSort(unsortedArr4, 0)}`);
+
+/**Merge Sort:
+ * Divide incoming list / array into smaller lists / arrays of one element. Repeatedly merge sublists
+ * to produce sorted sublists until there is only one sublist remaining which will be the sorted list.
+ */
+let i = 2;
+
+function mergeSort(arr){
+  let sublists = [];
+  arr.forEach((item) => sublists.push([item]));
+  for (i = 0; i < sublists.length; i++){
+    if (!sublists[i+1]){
+      continue;
+    }
+    if (sublists[i][0] > sublists[i+1][0]){
+      sublists[i] = [sublists[i+1][0], sublists[i][0]];
+    } else {
+      sublists[i] = [sublists[i][0], sublists[i+1][0]];
+    }
+    sublists.splice(i+1,1);
+  }
+  console.log(sublists);
+}
+
+mergeSort(unsortedArr5);
